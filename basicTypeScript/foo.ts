bf83067a7c76c14b576c417a6acaf522131c6d50
GeraldRyan <gerald.ryan40@protonmail.com>
2021-02-11 22:31:35 -0600

    put ts tutorial files in folder

---
+++ basicTypeScript/foo.ts
@@ -0,0 +1,30 @@
+class Grault {
+  private garply: string;
+
+  constructor(quux: Quux, waldo: number[]){
+    this.garply = quux.quuz + " " + quux.corge + " " + waldo
+  }
+
+  public getGarply(){
+    return this.garply
+  }
+}
+
+interface Quux {
+  quuz: string;
+  corge: number;
+}
+
+// function foo(bar: Quux) {
+//   return "Hello, " + bar.quuz + " " + bar.corge;
+// }
+
+let baz = <Quux> {
+  quuz: "ABC",
+  corge: 123,
+};
+
+let fred: Grault = new Grault(baz, [1,2,3])
+
+console.log(fred.getGarply());
+document.body.innerHTML = fred.getGarply()
